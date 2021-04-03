ee251b98201376d81442891ae8129fb7e68d63f9
AwesomeCoder343 <73550340+AwesomeCoder343@users.noreply.github.com>
2021-04-02 19:45:51 -0500

    Add files via upload

--- guitar.js
+++ guitar.js
@@ -1,14 +1,29 @@
-img="";
-status="";
-object=[];
-function setup(){
-    canvas=createCanvas(380, 380);
-    canvas.center();
-    video=createCapture(VIDEO);
-    video.hide();
-    objectdetector=ml5.objectDetector('cocosst', modelLoaded);
-    document.getElementById("status").innerHTML="Status: Detecting Objects";
+if(status!=""){
+    objectdetector.detect(video, gotResult);
+    for(i=0; i<object.length; i++){
+       document.getElementById("status").innerHTML="Status: Object detected";
+       document.getElementById("number_of_objects").innerHTML="Number of objects are: "+ object.length;
+       fill(r, g, b);
+       percent=floor(object[i].confidence*100);
+       text(object[i].label + " " + percent + "%", object[i].x, object[i].y);
+       noFill();
+       stroke(r,g,b);
+       rect(object[i].x, object[i].y, object[i].width, object[i].height);
+       
+    }
 }
-function preload(){
-    img=loadImage('guitar.jpg');
+
+function modelLoaded(){
+    console.log("Model is loaded!");
+    status=true;
+    
+}
+function gotResult(error, results){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    object=results;
+}
 }
